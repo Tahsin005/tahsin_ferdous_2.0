e7ae15c3c77ad0edebb792d4ea0aee23323c37d2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-12-16 17:51:45 +0600

    power exponentiation

--- Basic Maths/L5/1_power_exponentiation.cpp
+++ Basic Maths/L5/1_power_exponentiation.cpp
@@ -10,7 +10,13 @@ int main(){
     int x, n;
     cin >> x >> n;
 
-    int ans = 1;
+    double ans = 1;
+    bool isNegativePower = false;
+    if (n < 0) {
+        isNegativePower = true;
+    }
+
+    n = abs(n);
 
     // Time complexity - logN
 
@@ -23,6 +29,12 @@ int main(){
             x *= x;
         }
     }
+
+
+    if (isNegativePower) {
+        ans = 1 / ans;
+    }
+
     cout << ans << '\n';
     return 0;
 }
